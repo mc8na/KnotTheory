f4ad72fa0f7347cd8f530accc9a48df2afbaa4e0
mc8na <milesclikeman@gmail.com>
2018-05-26 18:13:29 -0400

    modified T class to support arithmetic

--- knots.py
+++ knots.py
@@ -10,16 +10,39 @@ class Crossing:
 	def __str__(self):
 		return '{self.index},{self.i},{self.j},{self.k},{self.i}'
 
+class T:
+	def __init__(self,c,e,i,n):
+		self.t = [0]*n
+		self.t[0] += i
+		self.t[e] += c
+	def __add__(self,other):
+		for i in range(len(self.t)):
+			self.t[i] += other.t[i]
+		return self
+	def __mul__(self,other):
+		new = T(0,0,0,len(self.t)+len(other.t)-1)
+		for i in range(len(self.t)):
+			for j in range(len(other.t)):
+				new[i+j] += self.t[i]*other.t[j]
+		return new	
+	def __str__(self):
+		s = ""
+		for i in range(len(self.t)):
+			s += str(i) + "t^" + str(self.t[i]) + " "
+		return s
+
 class Knot:
 	def __init__(self,pdcode):
 		self.d = {}
+		i = 1
 		for n in pdcode:
 			c = Crossing(n,len(pdcode)*2)
-			self.d[c.i] = c
+			self.d[i] = c
+			i += 1
 	def dtCode(self):
 		pairs = {}
 		for a in self.d:
-			if a%2 == 1:
+			if self.d[a].i%2 == 1:
 				if self.d[a].rhr > 0:
 					pairs[a] = -(self.d[a].l)
 				else:
@@ -39,8 +62,6 @@ class Knot:
 			t += 2*n-1
 			t += list[n-1]
 		gcode = [0] * len(t)
-		#for n in range(len(t)):
-		#	gcode += 0
 		for n in t:
 			if n%2 == 1: # n is odd
 				if n <= len(t)//2: # first time through a crossing
@@ -72,4 +93,23 @@ class Knot:
 		return Knot(self.d.copy())
 	def __str__(self):
 		return str(self.d)
+	def alexanderMatrix(self):
+		gcode = gaussCode(self)
+		m = [[T(0,0,0) for x in range(len(self.d))] for y in range(len(self.d))]
+		a = 1
+		for n in gcode:
+			if n > 0:
+				m[n][a] += T(-1,1,1)
+			elif self.d[n].rhr > 0:
+				m[n][a] += T(0,0,-1)
+				a += a%len(self.d) + 1
+				m[n][a] += T(1,1,0)
+			else:
+				m[n][a] += T(1,1,0)
+				a += a%len(self.d) + 1
+				m[n][a] += T(0,0,-1)
+		del m[len(m)-1]
+		for n in range(len(m)):
+			del m[n][len(m)]
+					
 		
