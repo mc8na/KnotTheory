b844aacdd598eb64043b0daf0d1bae31dcddb5de
mc8na <milesclikeman@gmail.com>
2018-05-28 16:17:34 -0400

    Tested and fixed dtCode(), gaussCode(), and aPoly(), now work on one example

--- knots.py
+++ knots.py
@@ -25,27 +25,47 @@ class T:
 	def __add__(self,other):
 		new = T(0,0,0,max(len(self.t),len(other.t)))
 		for i in range(len(self.t)):
-			new[i] += self.t[i]
+			new.t[i] += self.t[i]
 		for i in range(len(other.t)):
-			new[i] += other.t[i]
+			new.t[i] += other.t[i]
+		return new
+	def __iadd__(self,other):
+		new = T(0,0,0,max(len(self.t),len(other.t)))
+		for i in range(len(self.t)):
+			new.t[i] += self.t[i]
+		for i in range(len(other.t)):
+			new.t[i] += other.t[i]
 		return new
 	def __sub__(self,other):
 		new = T(0,0,0,max(len(self.t),len(other.t)))
 		for i in range(len(self.t)):
-			new[i] += self.t[i]
+			new.t[i] += self.t[i]
 		for i in range(len(other.t)):
-			new[i] -= other.t[i]
+			new.t[i] -= other.t[i]
+		return new
+	def __isub__(self,other):
+		new = T(0,0,0,max(len(self.t),len(other.t)))
+		for i in range(len(self.t)):
+			new.t[i] += self.t[i]
+		for i in range(len(other.t)):
+			new.t[i] -= other.t[i]
 		return new
 	def __mul__(self,other):
 		new = T(0,0,0,len(self.t)+len(other.t)-1)
 		for i in range(len(self.t)):
 			for j in range(len(other.t)):
-				new[i+j] += self.t[i]*other.t[j]
+				new.t[i+j] += self.t[i]*other.t[j]
+		return new
+	def __imul__(self,other):
+		new = T(0,0,0,len(self.t)+len(other.t)-1)
+		for i in range(len(self.t)):
+			for j in range(len(other.t)):
+				new.t[i+j] += self.t[i]*other.t[j]
 		return new
 	def __str__(self):
 		s = ""
 		for i in range(len(self.t)):
-			s += str(i) + "t^" + str(self.t[i]) + " "
+			s += str(self.t[i]) + "t^" + str(i) + " "
 		return s
 
 class GCode:
@@ -61,6 +81,8 @@ class GCode:
 				self.code += [(-1 if n < 0 else 1)*self.maxi]
 				self.maxi += 1
 		self.maxi -= 1
+	def __str__(self):
+		return str(self.code)
 # k = Knot( ( (index,i,j,k,l),(index,i,j,k,l)... ) , ( (reg1,reg2,reg3),(reg4,reg1)... ) )
 class Knot:
 	def __init__(self,pdcode,regions):
@@ -74,7 +96,7 @@ class Knot:
 		i = 1
 		for n in regions:
 			self.r[i] = n[:]
-	def rcc(self,i) # RCC move on region i
+	def rcc(self,i): # RCC move on region i
 		for n in self.r[i]:
 			self.d[n].cc()
 	def dtCode(self):
@@ -82,47 +104,48 @@ class Knot:
 		for a in self.d:
 			if self.d[a].i%2 == 1:
 				if self.d[a].rhr > 0:
-					pairs[a] = -(self.d[a].l)
+					pairs[self.d[a].i] = -(self.d[a].l)
 				else:
-					pairs[a] = -(self.d[a].j)
+					pairs[self.d[a].i] = -(self.d[a].j)
 			elif self.d[a].rhr > 0:
-				pairs[self.d[a].l] = a
+				pairs[self.d[a].l] = self.d[a].i
 			else:
-				pairs[self.d[a].j] = a
+				pairs[self.d[a].j] = self.d[a].i
 		dtcode = []
 		for a in sorted(pairs):
 			dtcode += [pairs[a]]
 		return dtcode
 	def gaussCode(self):
-		list = dtCode(self)
+		list = self.dtCode()
 		t = []
 		for n in range(1,len(list)+1):
 			t += [2*n-1]
 			t += [list[n-1]]
 		gcode = [0] * len(t)
-		for n in t:
-			if n%2 == 1: # n is odd
-				if n <= len(t)//2: # first time through a crossing
-					if t[n+1] > 0: # over crossing
-						gcode[n] = n
-					else:		   # under crossing
-						gcode[n] = -n
-				elif t[n+1] > 0:   # second time through a crossing
-					gcode[n] = t[n+1] # over the pair
-				else:				# under the pair
-					gcode[n] = -t[n+1]
-			else: #n is even
-				if n > 0 and n <= len(t)//2: # first time under
-					gcode[n] = -n
-				elif n > 0: # under crossing second time through
-					gcode[n] = -t[n-1]
-				elif abs(n) <= len(t)//2: # first time over
-					gcode[abs(n)] = abs(n)
-				else: # over crossing second time through
-					gcode[abs(n)] = t[n-1]
+		for n in range(len(t)):
+			if t[n]%2 == 1:
+				if t[n] <= len(t)//2:
+					if t[n+1] > 0:
+						gcode[t[n]-1] = t[n]
+					else:
+						gcode[t[n]-1] = -t[n]
+				elif t[n+1] > 0:
+					gcode[t[n]-1] = t[n+1]
+				else:
+					gcode[t[n]-1] = -t[n+1]
+			else:
+				if t[n] > 0:
+					if t[n] <= len(t)//2:
+						gcode[t[n]-1] = -t[n]
+					else:
+						gcode[t[n]-1] = -t[n-1]
+				elif abs(t[n]) <= len(t)//2:
+					gcode[abs(t[n])-1] = abs(t[n])
+				else:
+					gcode[abs(t[n])-1] = t[n-1]
 		return GCode(gcode)
 	def isAlternating(self):
-		dt = dtCode(self)
+		dt = self.dtCode()
 		for n in range(len(dt)):
 			if dt[n-1]*dt[n] < 0:
 			 return False
@@ -132,35 +155,35 @@ class Knot:
 	def __str__(self):
 		return str(self.d)
 	def aPoly(self):
-		gcode = gaussCode(self)
+		gcode = self.gaussCode()
 		m = [[T(0,0,0,1) for x in range(len(self.d))] for y in range(len(self.d))]
 		a = 1
 		for n in gcode.code:
 			if n > 0:
-				m[n][a] += T(-1,1,1,2)
-			elif self.d[n].rhr > 0:
-				m[n][a] += T(0,0,-1,2)
-				a += a%len(self.d) + 1
-				m[n][a] += T(1,1,0,2)
+				m[n-1][a-1] += T(-1,1,1,2)
+			elif self.d[abs(n)].rhr > 0:
+				m[abs(n)-1][a-1] += T(0,0,-1,1)
+				a = (a%len(self.d)) + 1
+				m[abs(n)-1][a-1] += T(1,1,0,2)
 			else:
-				m[n][a] += T(1,1,0)
-				a += a%len(self.d) + 1
-				m[n][a] += T(0,0,-1,1)
+				m[abs(n)-1][a-1] += T(1,1,0,2)
+				a = (a%len(self.d)) + 1
+				m[abs(n)-1][a-1] += T(0,0,-1,1)
 		del m[len(m)-1]
 		for n in range(len(m)):
 			del m[n][len(m)]
-		#apoly = det(m)
-		apoly = solve(m,1)
+		apoly = self.det(m)
+		#apoly = self.solve(m,T(0,0,1,1))
 		i = 0
 		while i < len(apoly.t) and apoly.t[i] == 0:
 			i += 1
 		if i > 0:
 			for j in range(i,len(apoly.t)):
-				apoly.t[j-i] = apoly.t[j]
+				apoly.t[j-i],apoly.t[j] = apoly.t[j],0
 		while len(apoly.t) > 1 and apoly.t[-1] == 0:
 			del apoly.t[-1]
 		return apoly
-	def det(l):
+	def det(self,l):
 		n = len(l)
 		if (n>2):
 			i,t,sum = T(0,0,1,1),0,T(0,0,0,1)
@@ -183,8 +206,8 @@ class Knot:
 				t += 1
 			return sum
 		else:
-			return(m[0][0]*m[1][1]-m[0][1]*m[1][0])
-	def solve(matrix,mul):
+			return(l[0][0]*l[1][1]-l[0][1]*l[1][0])
+	def solve(self,matrix,mul):
 		width = len(matrix)
 		if width == 1:
 			return mul * matrix[0][0]
@@ -200,7 +223,7 @@ class Knot:
 							buff.append(matrix[j][k])
 					m.append(buff)
 				sign *= T(0,0,-1,1)
-				total += mul * solve(m, sign * matrix[0][i])
+				total += mul * self.solve(m, sign * matrix[0][i])
 			return total
 	def rmoves(self,gcode): # simplifying moves only for now
 		moves = []
@@ -252,7 +275,7 @@ class Knot:
 				moves += [GCode(newcode)]
 		return moves
 	def simplify(self,verbose=False):
-		simp = gaussCode(self)
+		simp = self.gaussCode()
 		rlist = self.rmoves(simp)
 		tries = 0
 		while rlist and tries < 100:
